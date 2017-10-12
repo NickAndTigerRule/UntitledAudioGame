83461ae9a1828f0cdf0947fdec632987f997d461
NicholasDuxbury <nicholasduxbury@hotmail.com>
2017-10-12 14:22:24 +1100

    Added a shooting test script and changed some lighting problems in nicks scene, added a plasma particle effect (still needs work) - next on adgenda some level blocks/platforms/inclines/ramps etc.

---
+++ Assets/Scripts/FirePartices.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePartices : MonoBehaviour {
+    public GameObject player;
+    private GameObject curFire1;
+    public float force;
+    public GameObject fireBall;
+    public GameObject plasmaBall;
+    
+
+    // Use this for initialization
+    void Start () {
+        curFire1 = fireBall;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            curFire1 = fireBall;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            curFire1 = plasmaBall;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            var offset = new Vector3(player.transform.position.x, player.transform.position.y + 1, player.transform.position.z);
+            var projectile = Instantiate(curFire1, offset, transform.rotation);
+
+            projectile.GetComponent<Rigidbody>().AddForce(Vector3.right * force);
+            Destroy(projectile, 2f);
+        }
+
+	}
+}
