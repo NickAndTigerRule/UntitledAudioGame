b3d5ad26baf02d6134849185b7f150ff2735155c
NicholasDuxbury <nicholasduxbury@hotmail.com>
2017-10-13 01:13:55 +1100

    Added post processing package (large file) some effects broken atm. Wrote universal spectrum data script (can be tied to anything). ohh and also alot of pissing around...

---
+++ Assets/Scripts/AudioData.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioData : MonoBehaviour {
+
+    AudioSource audioSource;
+
+    private float[] samples = new float[512];
+    private float[] freqBands = new float[8];
+    private float[] bandBuffer = new float[8];
+    private float[] bufferDecrease = new float[8];
+    float[] freqBandMax = new float[8];
+    public static float[] audioBand = new float[8];
+    public static float[] audioBandBuffer = new float[8];
+
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update ()
+    {
+        GetSpectrumData();
+        FrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+    }
+
+
+    ////////////////////////////////////////////////////////////////
+    ////////////////////////////Functions///////////////////////////
+    ////////////////////////////////////////////////////////////////
+
+    //Get the spectrum data from all samples
+    void GetSpectrumData()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if(freqBands[i] > freqBandMax[i])
+            {
+                freqBandMax[i] = freqBands[i];
+            }
+            audioBand[i] = (freqBands[i] / freqBandMax[i]);
+            audioBandBuffer[i] = (bandBuffer[i] / freqBandMax[i]);
+        }
+    }
+
+    //Split samples into frequency bands
+    void FrequencyBands()
+    {
+        var size = 0;
+        var average = 0f;
+
+        for (int i = 0; i < 8; i++)
+        {
+            int sampleSize = (int)Mathf.Pow(2, i) * 2;
+
+            if(i == 7)
+            {
+                sampleSize += 2;
+            }
+
+            for (int j = 0; j < sampleSize; j++)
+            {
+                average += samples[size] * (size + 1);
+                size++;
+                freqBands[i] = average * 10;
+            }
+        }
+    }
+
+    void BandBuffer()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if(freqBands[i] > bandBuffer[i])
+            {
+                bandBuffer[i] = freqBands[i];
+                bufferDecrease[i] = 0.005f;
+            }
+            if(freqBands[i] < bandBuffer[i])
+            {
+                bandBuffer[i] -= bufferDecrease[i];
+                bufferDecrease[i] *= 1.2f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+
+    public int band;
+    public float startScale;
+    public float scaleMulti;
+
+    Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().materials[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioData.audioBandBuffer[band] * scaleMulti) + startScale, transform.localScale.z);
+        Color colour = new Color(AudioData.audioBandBuffer[band], AudioData.audioBandBuffer[band], AudioData.audioBandBuffer[band]);
+        mat.SetColor("_EmissionColor", colour);
+	}
+}
