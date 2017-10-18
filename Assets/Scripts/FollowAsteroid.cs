eb1bf1c7637cdab838d2d71b61d27303732b282b
SAE\nduxbury <110317@student.sae.edu.au>
2017-10-18 20:42:36 +1000

    Asterockets

---
+++ Assets/Scripts/FollowAsteroid.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAsteroid : MonoBehaviour {
+    public GameObject asteroid;
+    public float force;
+	void Update () {
+        this.gameObject.GetComponent<Rigidbody>().AddForce(-Vector3.up * force);
+	}
+}
