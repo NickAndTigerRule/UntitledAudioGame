4c7ed76e832b742388e6c2915042e33721897aaa
SAE\nduxbury <110317@student.sae.edu.au>
2017-10-18 19:02:27 +1000

    Particles and models for moon

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+    public Light myLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var value = Mathf.Lerp(Random.Range(0.3f, 0.6f), Random.Range(0.8f, 1.1f), 1);
+        myLight.GetComponent<Light>().intensity = value;
+	}
+}
